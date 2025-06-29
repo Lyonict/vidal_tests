cca3390a2fed8b14c04cdc811f1b5c4c8c4da492
Charles <charles42.h@gmail.com>
2025-06-29 13:48:47 +0200

    Test 4 : improve security and logic of apiCall

--- src/ApiService.php
+++ src/ApiService.php
@@ -1,14 +1,27 @@
 <?php
 
+// Not sure if there's a better way to handle errors for a curl call...
+
 class ApiService
 {
     public function apiCall(Product $product): void
     {
         $url = "https://api.vidal.fr/rest/api/product/{$product->getId()}";
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt_array($ch, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+            // We would also add the api token here
+        ]);
         $res = curl_exec($ch);
+
+        if (!$res) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            trigger_error('Curl error', E_USER_WARNING);
+            return;
+        }
+
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
@@ -16,8 +29,12 @@ class ApiService
             trigger_error('API Call error', E_USER_WARNING);
             return;
         }
-
         $xml = simplexml_load_string($res);
+        if ($xml === false) {
+            trigger_error('Invalid XML response', E_USER_WARNING);
+            return;
+        }
+
         $product->setId(strval($xml->entry->id));
         $product->setName(isset($xml->entry->name) ? (string) $xml->entry->name : '');
         $product->setMarketStatus((string) $xml->entry->marketStatus);
